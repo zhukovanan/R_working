11365ee35f1e98473c5f7e674edda15776c2ecf6
zhukovanan <75267969+zhukovanan@users.noreply.github.com>
2020-12-27 20:18:25 +0300

    Update Wildberries api connect.R

--- Wildberries api connect.R
+++ Wildberries api connect.R
@@ -5,86 +5,30 @@ library(odbc)
 library(data.table)
 library(jsonlite)
 library(httr)
+library(lubridate)
+library(taskscheduleR)
 
+taskscheduler_create(taskname = "myscriptdaily", rscript = 'C:/Users/a.shukov/Desktop/Prog/R/Wildberries_api_connect.R' , 
+                     schedule = "DAILY", starttime = "20:08", 
+                     startdate = format(Sys.Date(), "%d/%m/%Y"))
+tasks <- taskscheduler_ls()
+str$)
 #Form named api vector 
 api.par <- c("ГЕМ" = "NDY4Zjk0NjQtNTBiOC00MWM5LWE4MDQtOWI4M2Y5ZTQ1MTAz" , "КТ" = "Y2ViOTVkNTgtZjUyOS00ZmY4LWI3YjYtZjhjYjM3NTQzNWRk"
 , "КСВ" = "MmVkOGZkZTgtYjVmNi00ZTk4LWJhYzMtMWI5ZWU5MDVmZTNi", "МОДЖИО" = "ZGYzY2VkYmUtMDI5MC00ZTU2LWI4YmUtMDNlZTA5MGIzYjgy",
 "МЕВ" = "NDJjZTM3OWMtZDQ1Yi00ZDY1LTkwMGItODA3MjQ4ODY1MzA4" , "МАС" = "ODRlOTNiMTEtNTg1OC00ZDg1LTlkZDYtZmI3ODU2OTFiMmVm" ,
  "УРК" = "MGI0NzhkYjMtYjg1Yy00Nzg2LWI2MTUtMDQxMWYzMmI3Nzli")
 
+path <- "https://suppliers-stats.wildberries.ru/api/v1/supplier/"
 #Make functions for different api tables
 #Sales
-sales.api <- function(x) {
-  res <- GET("https://suppliers-stats.wildberries.ru/api/v1/supplier/sales", query = list(dateFrom = "2020-01-01" ,flag = 0, key = x))
+wb.api <- function(key, data , date = Sys.Date(), dateto = Sys.Date()+1 ) {
+  url <- paste0(path , data)
+  res <- GET(url, query = list( dateFrom = date ,dateto = dateto,  flag = 0, key = key))
   resp_char <- rawToChar(res$content)
   Encoding(resp_char) <- "UTF-8"
   df <-  as.data.frame(fromJSON(resp_char))
-  tryCatch(df$Vendor <- names(api.par[api.par == x]), error=function(e) NULL)
-  if (nrow(df) == 0) {
-    return(NULL) 
-  } else {
-    return(df)
-  }
-}
-#Orders
-orders.api <- function(x) {
-  res <- GET("https://suppliers-stats.wildberries.ru/api/v1/supplier/orders", query = list(dateFrom = "2020-01-01" ,flag = 0, key = x))
-  resp_char <- rawToChar(res$content)
-  Encoding(resp_char) <- "UTF-8"
-  df <-  as.data.frame(fromJSON(resp_char))
-  tryCatch(df$Vendor <- names(api.par[api.par == x]), error=function(e) NULL)
-  if (nrow(df) == 0) {
-    return(NULL) 
-  } else {
-    return(df)
-  }
-  }
-#Report by daily 
-Realisation.api <- function(x) {
-  res <- GET("https://suppliers-stats.wildberries.ru/api/v1/supplier/reportDetailByPeriod", query = list(dateFrom = "2020-09-01" , key = x, dateto = "2020-12-21"))
-  resp_char <- rawToChar(res$content)
-  Encoding(resp_char) <- "UTF-8"
-  df <-  as.data.frame(fromJSON(resp_char))
-  tryCatch(df$Vendor <- names(api.par[api.par == x]), error=function(e) NULL)
-  if (nrow(df) == 0) {
-    return(NULL) 
-  } else {
-    return(df)
-  }
-}
-#Wayrhouse_cost
-Warehouse.api <- function(x) {
-  res <- GET("https://suppliers-stats.wildberries.ru/api/v1/supplier/stochrancost", query = list(dateFrom = "2020-09-01" , key = x, dateto = "2020-12-21"))
-  resp_char <- rawToChar(res$content)
-  Encoding(resp_char) <- "UTF-8"
-  df <-  as.data.frame(fromJSON(resp_char))
-  tryCatch(df$Vendor <- names(api.par[api.par == x]), error=function(e) NULL)
-  if (nrow(df) == 0) {
-    return(NULL) 
-  } else {
-    return(df)
-  }
-}
-#Stock
-stock.api <- function(x) {
-  res <- GET("https://suppliers-stats.wildberries.ru/api/v1/supplier/stocks", query = list(dateFrom = "2017-03-25T21:00:00.000Z" , key = x))
-  resp_char <- rawToChar(res$content)
-  Encoding(resp_char) <- "UTF-8"
-  df <-  as.data.frame(fromJSON(resp_char))
-  tryCatch(df$Vendor <- names(api.par[api.par == x]), error=function(e) NULL)
-  if (nrow(df) == 0) {
-    return(NULL) 
-  } else {
-    return(df)
-  }
-}
-#Incomes
-incomes.api <- function(x) {
-  res <- GET("https://suppliers-stats.wildberries.ru/api/v1/supplier/incomes", query = list(dateFrom = "2017-03-25T21:00:00.000Z" , key = x))
-  resp_char <- rawToChar(res$content)
-  Encoding(resp_char) <- "UTF-8"
-  df <-  as.data.frame(fromJSON(resp_char))
-  tryCatch(df$Vendor <- names(api.par[api.par == x]), error=function(e) NULL)
+  tryCatch(df$Vendor <- names(api.par[api.par == key]), error=function(e) NULL)
   if (nrow(df) == 0) {
     return(NULL) 
   } else {
@@ -93,19 +37,18 @@ incomes.api <- function(x) {
 }
 
 #Based on api data form necessary data frames
-Orders_WB <- do.call(dplyr::bind_rows, lapply(api.par, orders.api)) 
-Sales_WB <- do.call(dplyr::bind_rows, lapply(api.par, sales.api))
-Realisation_WB <- do.call(dplyr::bind_rows, lapply(api.par, Realisation.api))
-Warehouse_cost_WB <- do.call(dplyr::bind_rows, lapply(api.par, Warehouse.api))
-Stock_WB <- do.call(dplyr::bind_rows, lapply(api.par, stock.api))
-Incomes_WB <- do.call(dplyr::bind_rows, lapply(api.par, incomes.api)) 
+Sales_WB <- do.call(dplyr::bind_rows, lapply(api.par, wb.api, "sales" ) )
+Orders_WB <- do.call(dplyr::bind_rows, lapply(api.par, wb.api,"orders"))
+Realisation_WB <- do.call(dplyr::bind_rows, lapply(api.par, wb.api,"reportDetailByPeriod", "2020-12-01" ))
+Stock_WB <- do.call(dplyr::bind_rows, lapply(api.par, wb.api, "stocks" ))
 
 #Connect to SQL Server
 
 con <- dbConnect(odbc::odbc(), Driver = "SQL Server", Server = "SQL002\\MSSQL002", 
-                 Database = "report_kr", UID = "report", PWD = rstudioapi::askForPassword("Database password"), encoding = "1251)
+                 Database = "report_kr", UID = "report", PWD = "shitcode", encoding = "1251")
 
-#Try convert to appropriate data type 
-df$date <- as.Date.character(df$date)
-
-dbCreateTable(con,"WB_supply", df[,1:13])
+#Fresh sql data
+dbWriteTable(con, "Sales_WB" , Sales_WB, append = TRUE)
+dbWriteTable(con, "Orders_WB" , Orders_WB, append = TRUE)
+dbWriteTable(con, "Realisation_WB" , Realisation_WB, append = TRUE)
+dbWriteTable(con, "Stock_WB" , Stock_WB, append = TRUE)
